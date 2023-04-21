5eca081f313c6f0a0efdc838499a4b9c6cfbe5bd
zhengyiliang <zheng.yiliang@qq.com>
2023-04-21 17:28:51 +0800

    chore: update

--- components/index.ts
+++ components/index.ts
@@ -1,5 +1,6 @@
 // 文档发布的时候这个放开
-import './style/index.less';
+// npm 发布的时候隐藏
+// import './style/index.less';
 
 export { default as Button } from './Button';
 export { default as Space } from './Space';
